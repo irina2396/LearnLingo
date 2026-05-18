673efeb74178e146a999ceabc0fd28b3af5c6f27
Iryna Ponikarchyk <irishka.ponikarchik@ukr.net>
2026-05-18 12:53:40 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,22 +5,29 @@ export default function Header() {
     return (
         <header className={css.header}>
             <div className={`container ${css.headerContainer}`}>
-                <Link className={css.logo} to="/">
-                    <svg className={css.logoIcon} width={28} height={28}>
-                        <use href="/symbol-defs.svg#icon-ukraine"></use>
-                    </svg>
-                    LearnLingo
-                </Link>
-                <nav className={css.navigation}>
-                    <Link to="/">Home</Link>
-                    <Link to="">Teachers</Link>
+                <nav className={css.headerNav}>
+                    <Link className={css.logo} to="/">
+                        <svg className={css.logoIcon} width={28} height={28}>
+                            <use href="/symbol-defs.svg#icon-ukraine"></use>
+                        </svg>
+                        LearnLingo
+                    </Link>
+                        <ul className={css.navList}>
+                            <li className={css.navItem}><Link className={css.navLink} to="/">Home</Link></li>
+                            <li className={css.navItem}><Link className={css.navLink} to="/teachers">Teachers</Link></li>
+                        </ul>
                 </nav>
-                <div className={css.authButtons}>
-                    <svg className={css.iconLogin} width={20} height={20}>
-                        <use className={css.loginIcon} href="/symbol-defs.svg#icon-log-in-01"/>
-                    </svg>
-                    <button className={css.authButton} type="button">Log in</button>
-                    <button className={css.authButton} type="button">Registration</button>
+                <div className={css.authItem}>
+                    <button className={css.buttonLogin} type="button">
+                        <svg className={css.iconLogin} width={20} height={20}>
+                            <use href="/symbol-defs.svg#icon-log-in-01" />
+                        </svg>
+                        Log in
+                    </button>
+
+                    <button className={css.buttonRegistration} type="button">
+                        Registration
+                    </button>
                 </div>
             </div>
         </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './components/App/App.tsx'
+import "modern-normalize"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 )
+
+
