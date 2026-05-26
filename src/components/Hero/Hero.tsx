982fba07394688e12a4a51b2806d66d8ab4e4e71
Iryna Ponikarchyk <irishka.ponikarchik@ukr.net>
2026-05-26 11:52:58 +0300

    fix hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,14 +4,18 @@ import css from "./Hero.module.css"
 export default function Hero() {
     return (
         <section className={css.hero}>
-            <div className={css.container}>
-                <h1 className={css.title}>Unlock your potential with the best  language tutors</h1>
-                <p className={css.text}>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
-                <Button>Get started</Button>
-                <div>
-                    <img src="/public/block.png" alt="teacher"/>
+                <div className="container">
+                    <div className={css.heroContent}>
+                        <div className={css.pageHero}>
+                            <h1 className={css.title}>Unlock your potential with the best  <span className={css.titlePart}>language</span> tutors</h1>
+                            <p className={css.text}>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
+                            <Button>Get started</Button>
+                        </div>
+                        <div className={css.imgWrapper}>
+                            <img className={css.image} src="/public/block.png" alt="teacher"/>
+                        </div>
+                    </div>
                 </div>
-            </div>
         </section>
     )
 }
